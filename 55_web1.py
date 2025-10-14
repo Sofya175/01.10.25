2f96ad2bda26358f931960e743ca362a89b4b3cc
Sofya175 <zueva-sv@mail.ru>
2025-10-15 00:45:41 +0300

    Commit_web

--- 55_web1.py
+++ 55_web1.py
@@ -14,27 +14,31 @@
 from flask import Flask, url_for
 import sqlite3
 
-app = Flask(__name__) # name - модуль Питона 55_web1
+app = Flask(__name__)  # name - модуль Питона 55_web1
 
-@app.route('/') # декоратор для функции index()
+
+@app.route('/')  # декоратор для функции index()
 @app.route('/home')
 def index():
     return """Я ваше первое приложение.
     <br> Подробнее <a href="/about">здесь</a>
     <br> А вот <a href="/countdown">тут</a> обратный отсчет"""
 
-@app.route('/about') # декоратор
+
+@app.route('/about')  # декоратор
 def about():
     return """Эта страница с более подробной информацией.
     <br>А <a href="/genres"> вот тут </a> про жанры.
     <br>А <a href="/flag"> вот тут </a> будет флаг.
     <br> Подробнее <a href="/home">назад</a>"""
 
+
 @app.route('/countdown')
 def cdown():
     lst = [str(x) for x in reversed(range(11))]
     lst.append('Поехали!')
-    return '<br>'.join(lst) # join - соединение строк из списка
+    return '<br>'.join(lst)  # join - соединение строк из списка
+
 
 @app.route('/genres')
 def genres():
@@ -53,8 +57,8 @@ def genres():
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Жанры</title>
     <link rel="stylesheet" href="{url_for('static', filename="css/styles.css")}">
+    <title>Жанры</title>
 </head>
 <body>
 <h1>А вот и жанры:</h1>
@@ -64,6 +68,7 @@ def genres():
 </body>
 </html>"""
     return result
+
 
 @app.route('/flag')
 def flag():
@@ -75,27 +80,30 @@ def flag():
             height="40" width"60"
             alt=\"Нету флага\">"""
 
+# <name> - строка
+# <num:int> - целое
+# <num:float> - дес. дробь
 @app.route('/greet/', defaults={'name': None})
-# @app.route('/greet/<name>')
+@app.route('/greet/<name>')
 def greeting(name=None):
     if name is None:
         return '<h1>Не с кем здороваться!!!</h1>'
     return f"""
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="ru">
 <head>
-<!-- как будет отображаться на странице-->
+<!--    как будет отображаться на странице-->
     <meta charset="UTF-8">
     <meta name="description" content="Описание страницы сайта.">
-    <link rel="stylesheet" href="{url_for('static', filename="css/styles.css")}">
-    <title>Жанры</title>
+    <link rel="stylesheet" href="{url_for('static', filename='css/styles.css')}">
+    <title>Приветствуем тебя, {name}</title>
 </head>
 <body>
 <h1>{name.capitalize()}, мы приветствуем тебя</h1>
 </body>
-</html>"""
+</html>
+    """
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000) # host или локальный хост, 5000 - классический порт для приложения flask
-
+    app.run(host='127.0.0.1', port=5000)  # host или локальный хост, 5000 - классический порт для приложения flask
